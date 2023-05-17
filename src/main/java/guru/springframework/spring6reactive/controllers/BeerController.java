a355d7b7d14f552e90d9f637ee5aa2f6c92eae0c
Gabor Ballabas <gbr666@gmail.com>
2023-05-17 22:23:28 +0200

    Implement Flux from database

---
+++ src/main/java/guru/springframework/spring6reactive/controllers/BeerController.java
@@ -0,0 +1,22 @@
+package guru.springframework.spring6reactive.controllers;
+
+import guru.springframework.spring6reactive.model.BeerDTO;
+import guru.springframework.spring6reactive.services.BeerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+@RequiredArgsConstructor
+public class BeerController {
+
+    public static final String BEER_PATH = "/api/v2/beer";
+
+    private final BeerService beerService;
+
+    @GetMapping(BEER_PATH)
+    Flux<BeerDTO> listBeers() {
+        return beerService.listBeers();
+    }
+}

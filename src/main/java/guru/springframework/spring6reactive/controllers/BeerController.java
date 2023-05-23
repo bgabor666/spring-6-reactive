faf25f87c55574cda53618f67ba93e4409f65f23
Gabor Ballabas <gbr666@gmail.com>
2023-05-23 21:07:06 +0200

    Implement Patch assignment

--- src/main/java/guru/springframework/spring6reactive/controllers/BeerController.java
+++ src/main/java/guru/springframework/spring6reactive/controllers/BeerController.java
@@ -3,9 +3,11 @@ package guru.springframework.spring6reactive.controllers;
 import guru.springframework.spring6reactive.model.BeerDTO;
 import guru.springframework.spring6reactive.services.BeerService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 /**
  * Created by jt, Spring Framework Guru.
@@ -15,8 +17,38 @@ import reactor.core.publisher.Flux;
 public class BeerController {
 
     public static final String BEER_PATH = "/api/v2/beer";
+    public static final String BEER_PATH_ID = BEER_PATH + "/{beerId}";
 
     private final BeerService beerService;
+
+    @PatchMapping(BEER_PATH_ID)
+    Mono<ResponseEntity<Void>> patchBeer(@PathVariable("beerId") Integer beerId,
+                                         @RequestBody BeerDTO beerDTO) {
+        return beerService.patchBeer(beerId, beerDTO)
+                .map(savedDto -> ResponseEntity.ok().build());
+    }
+
+    @PutMapping(BEER_PATH_ID)
+    Mono<ResponseEntity<Void>> updateExistingBeer(@PathVariable("beerId") Integer beerId,
+                                                  @RequestBody BeerDTO beerDTO) {
+        return beerService.updateBeer(beerId, beerDTO)
+                .map(savedDto -> ResponseEntity.ok().build());
+    }
+
+    @PostMapping(BEER_PATH)
+    Mono<ResponseEntity<Void>> createNewBeer(@RequestBody BeerDTO beerDTO) {
+        return beerService.saveNewBeer(beerDTO)
+                .map(savedDto -> ResponseEntity.created(UriComponentsBuilder
+                        .fromHttpUrl("http://localhost:8080/" + BEER_PATH
+                                + "/" + savedDto.getId())
+                        .build().toUri())
+                        .build());
+    }
+
+    @GetMapping(BEER_PATH_ID)
+    Mono<BeerDTO> getBeerById(@PathVariable("beerId") Integer beerId) {
+        return beerService.getBeerById(beerId);
+    }
 
     @GetMapping(BEER_PATH)
     Flux<BeerDTO> listBeers(){
